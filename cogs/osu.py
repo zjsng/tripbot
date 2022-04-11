f5fe5c97a05a51c330f05e563afb2645be3870bb
zjsng <zijiesng@gmail.com>
2022-04-11 21:27:41 +0800

    Improved on the formatting for the osutop command
    Looks good now, requires some adjustment to dynamically using the
    correct uesr profile instead of hardcoded link

--- cogs/osu.py
+++ cogs/osu.py
@@ -18,24 +18,29 @@ class osu(commands.Cog):
 
         # Call on the API for the required information
         # response will be multiple dictionaries depending on the limit provided in params
+        url = f'{osuAPI.get_api_url()}/users/8497340/scores/best'
         params = osuAPI.get_params(0, 'osu', 5, int(offset) * 5)
-        response = await osuAPI.get_response(f'{osuAPI.get_api_url()}/users/8497340/scores/best', params=params)
+        response = await osuAPI.get_response(url, params=params)
 
         # Formatting the embed's message with the response from osu! API
-        count = (int(offset) * 5) + 1
-        embed_msg = discord.Embed(
-            title="Top Plays",
-            description=f"Showing page {int(offset) + 1} of the user's top plays",
-            colour=ctx.author.roles[-1].colour)
-        embed_msg.set_footer(text=ctx.author)
         value_list = []
+        count = (int(offset) * 5) + 1
         for dict in response.json():
             value_list.append("**{}. [{} [{}] ]({})** [{}★]\n{}pp".format(
                 count, dict['beatmapset']['title'], dict['beatmap']['version'], dict['beatmap']['url'],
                 dict['beatmap']['difficulty_rating'], dict['pp']
             ))
             count += 1
-        embed_msg.add_field(name=f"Top {int(count)-1} osu! Standard Plays", value='\n'.join(value_list), inline=False)
+        embed_msg = discord.Embed(
+            description='\n'.join(value_list),
+            colour=ctx.author.roles[-1].colour
+        )
+        embed_msg.set_author(
+            name=f"{ctx.author.name}'s top plays",
+            url="https://osu.ppy.sh/users/8497340",
+            icon_url=ctx.author.avatar_url
+        )
+        embed_msg.set_footer(text=ctx.author)
 
         await ctx.channel.send(embed=embed_msg)
 
